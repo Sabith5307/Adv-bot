0885b7e902ee4646f6b893a3e1d67e8c16ed59a4
Sabith5307 <87589910+Sabith5307@users.noreply.github.com>
2021-07-20 18:58:20 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,7 +28,7 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption ==@MoviesTalkies23,
+                caption =caption,
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
